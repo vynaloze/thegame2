6f3e1c60ec1507720bce2d2acab84031a339f915
Piotr Pawluk <piotr.pawluk@tomtom.com>
2020-02-28 14:38:00 +0100

    hello world

---
+++ src/main/kotlin/com/vynaloze/thegame2/TheGame2Application.kt
@@ -0,0 +1,11 @@
+package com.vynaloze.thegame2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheGame2Application
+
+fun main(args: Array<String>) {
+    runApplication<TheGame2Application>(*args)
+}
